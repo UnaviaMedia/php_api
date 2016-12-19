96c51b6cd3906e77aab395f7fba9b3d362b167bf
kroth5183 <kroth5183@conestogac.on.ca>
2016-12-19 15:03:17 -0500

    Add basic documentation to index page

--- index.php
+++ index.php
@@ -3,6 +3,51 @@ require_once("/home/cabox/workspace/constants.php");
 require_once(FRAGMENT_HEADER);
 ?>
 
+<div class="row">
+    <div class="column small-12 medium-6">
+        <h2 class="text-center">Request Types</h2>
+        <ul>
+            <li>POST<br />
+                <small>Create</small>
+            </li>
+            <li>GET<br />
+                <small>Read</small>
+            </li>
+            <li>PUT<br />
+                <small>Update</small>
+            </li>
+            <li>DELETE<br />
+                <small>Delete</small>
+            </li>
+        </ul>
+    </div>
+    <div class="column small-12 medium-6">
+        <h2 class="text-center">Use Cases</h2>
+        <dl>
+            <dd>Retrieve a country</dd>
+            <dt>
+                <pre>GET - /api/v1/countries/23</pre>
+            </dt>
+            <dd>Retrieve all countries</dd>
+            <dt>
+                <pre>GET - /api/v1/countries/list</pre>
+            </dt>
+            <dd>Add a new country</dd>
+            <dt>
+                <pre>POST - /api/v1/countries { name: "test" }</pre>
+            </dt>
+            <dd>Update a country</dd>
+            <dt>
+                <pre>PUT - /api/v1/countries { name: "another" }</pre>
+            </dt>
+            <dd>Remove a country</dd>
+            <dt>
+                <pre>DELETE - /api/v1/countries/23 { name: test }</pre>
+            </dt>
+        </dl>
+    </div>
+</div>
+
 <!-- URL for API -->
 <div class="row">
     <form action="#" class="column small-12 medium-6">
