5ed5c8a5676924bdd124c960a37a31f840165c10
UnaviaMedia <kroth@unaviamedia.ca>
2016-12-22 16:02:07 -0500

    Add sample create method to Continents controller

--- controller/con_continents.php
+++ controller/con_continents.php
@@ -2,6 +2,28 @@
 require_once("/home/cabox/workspace/constants.php");
 require_once(DAL . "/dal_continents.php");
 
-function readCountries() {
-    Continent::readAll();
+function readContinents() {
+	Continent::readAll();
+}
+
+function createContinent($name) {
+	//Create and validate the record
+	$result = new Continent("", $name);
+
+	if ( $result["status"] == 0 ) {
+		//Successful creation
+	} else {
+		//Return an error or warning
+		return;
+	}
+
+	//Add the record to the database
+	$result = Continent::create($continent);
+
+	if ( $result["status"] == 0) {
+		//Successful insertion
+	} else {
+		//Error or warning
+		return;
+	}
 }
